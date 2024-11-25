183727d065dc8026505135d207ee5c36124c10a6
Andrejev Denis <andrejevdenis00@gmail.com>
2024-11-25 09:29:29 +0400

    Seems like done

--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -12,6 +12,7 @@ def test_complete_todo():
     browser.element('[placeholder="Mobile Number"]').type(random.randint(1000000000, 9000000000)).press_tab()
     browser.element('[id="dateOfBirthInput"]').type(fake.date_of_birth(None, 1, 90)).press_enter()
 
+
     browser.element('[id="subjectsInput"]').type('M')
     browser.element('[id="react-select-2-option-0"]').click()
     browser.element('[id="subjectsInput"]').type('Co')
@@ -25,14 +26,17 @@ def test_complete_todo():
 
     browser.element('[for="uploadPicture"]').click()
     keyboard.write('https://github.com/andrejevdenis/Hw4_AndreevDk/blob/master/tests/Octopus.jpg')
-    keyboard.press('ENTER')
+    keyboard.send('ENTER')
 
     browser.element('[placeholder="Current Address"]').type(fake.address())
 
     browser.element('[id="state"]').click()
     browser.element('[id="react-select-3-option-0"]').click()
+    browser.element('[id="state"]').should(have.text('NCR'))
 
     browser.element('[id="city"]').click()
     browser.element('[id="react-select-4-option-0"]').click()
+    browser.element('[id="city"]').should(have.text('Delhi'))
 
     browser.element('[id="submit"]').click()
+    browser.element('[class="modal-header"]').should(have.text('Thanks for submitting the form'))
