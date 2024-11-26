a57376e43635b3ef4543c5e6b659d96fe5e287f7
Andrejev Denis <andrejevdenis00@gmail.com>
2024-11-26 11:39:57 +0400

    Seems like done

--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -17,8 +17,7 @@ def test_complete_todo():
     browser.element('[id="react-select-2-option-0"]').click()
     browser.element('[id="subjectsInput"]').type('Co')
     browser.element('[id="react-select-2-option-0"]').click()
-    browser.element('[id="subjectsContainer"]').should(have.text('Maths'))
-    browser.element('[id="subjectsContainer"]').should(have.text('Computer Science'))
+
 
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
@@ -32,10 +31,14 @@ def test_complete_todo():
 
     browser.element('[id="state"]').click()
     browser.element('[id="react-select-3-option-0"]').click()
-    browser.element('[id="state"]').should(have.text('NCR'))
+
 
     browser.element('[id="city"]').click()
     browser.element('[id="react-select-4-option-0"]').click()
+
+    browser.element('[id="subjectsContainer"]').should(have.text('Maths'))
+    browser.element('[id="subjectsContainer"]').should(have.text('Computer Science'))
+    browser.element('[id="state"]').should(have.text('NCR'))
     browser.element('[id="city"]').should(have.text('Delhi'))
 
     browser.element('[id="submit"]').click()
