c72410fb7c11172e8cfbfc8a7f96cf721c633d7b
Andrejev Denis <andrejevdenis00@gmail.com>
2024-11-26 12:37:57 +0400

    Podgon

--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -1,42 +1,33 @@
-import keyboard
+import os.path
+
 from selene import browser, have, be
-from faker import Faker
-fake = Faker()
-import random
 
 def test_complete_todo():
-    browser.element('[placeholder="First Name"]').type(fake.first_name_male())
-    browser.element('[placeholder="Last Name"]').type(fake.last_name_male())
-    browser.element('[id="userEmail"]').type(fake.email())
+    browser.element('[id="firstName"]').type('Fedor')
+    browser.element('[id="lastName"]').type('Lomovoy')
+    browser.element('[id="userEmail"]').type('fedyaosminog@ramb.com')
     browser.element('[for="gender-radio-1"]').click() #Male
-    browser.element('[placeholder="Mobile Number"]').type(random.randint(1000000000, 9000000000)).press_tab()
-    browser.element('[id="dateOfBirthInput"]').type(fake.date_of_birth(None, 1, 90)).press_enter()
-
+    browser.element('[id="userNumber"]').type('1000000000').press_tab()
+    browser.element('[id="dateOfBirthInput"]').type(23-10-2000).press_enter()
 
     browser.element('[id="subjectsInput"]').type('M')
     browser.element('[id="react-select-2-option-0"]').click()
     browser.element('[id="subjectsInput"]').type('Co')
     browser.element('[id="react-select-2-option-0"]').click()
-    browser.element('[id="subjectsContainer"]').should(have.text('Maths'))
-    browser.element('[id="subjectsContainer"]').should(have.text('Computer Science'))
 
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
     browser.element('[for="hobbies-checkbox-3"]').click()
 
-    browser.element('[for="uploadPicture"]').click()
-    keyboard.write('https://github.com/andrejevdenis/Hw4_AndreevDk/blob/master/tests/Octopus.jpg')
-    keyboard.send('ENTER')
+    browser.element('[id="uploadPicture"]').set_value(os.path.abspath("../tests/Octopus.jpg"))
 
-    browser.element('[placeholder="Current Address"]').type(fake.address())
+    browser.element('[id="currentAddress"]').type('Moscow, Bulkina 65-39')
 
     browser.element('[id="state"]').click()
     browser.element('[id="react-select-3-option-0"]').click()
-    browser.element('[id="state"]').should(have.text('NCR'))
 
     browser.element('[id="city"]').click()
     browser.element('[id="react-select-4-option-0"]').click()
-    browser.element('[id="city"]').should(have.text('Delhi'))
 
     browser.element('[id="submit"]').click()
     browser.element('[class="modal-header"]').should(have.text('Thanks for submitting the form'))
