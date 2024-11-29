b9da6f529ed38ec8f82dc8569c4b2af70a990010
Andrejev Denis <andrejevdenis00@gmail.com>
2024-11-29 18:53:15 +0400

    Perhaps don would be glad o_O

--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -8,7 +8,9 @@ def test_complete_todo():
     browser.element('[id="userEmail"]').type('fedyaosminog@ramb.com')
     browser.element('[for="gender-radio-1"]').click() #Male
     browser.element('[id="userNumber"]').type('1000000000').press_tab()
-    browser.element('[id="dateOfBirthInput"]').type(23-10-2000).press_enter()
+    browser.element('[class="react-datepicker__month-select"]').element('[value="9"]').click()
+    browser.element('[class="react-datepicker__year-select"]').element('[value="2000"]').click()
+    browser.element('[class="react-datepicker__day react-datepicker__day--023"]').click()
 
     browser.element('[id="subjectsInput"]').type('M')
     browser.element('[id="react-select-2-option-0"]').click()
@@ -31,3 +33,13 @@ def test_complete_todo():
 
     browser.element('[id="submit"]').click()
     browser.element('[class="modal-header"]').should(have.text('Thanks for submitting the form'))
+    browser.element('[class="table-responsive"]').should(have.text('Fedor Lomovoy'))
+    browser.element('[class="table-responsive"]').should(have.text('fedyaosminog@ramb.com'))
+    browser.element('[class="table-responsive"]').should(have.text('Male'))
+    browser.element('[class="table-responsive"]').should(have.text('1000000000'))
+    browser.element('[class="table-responsive"]').should(have.text('23 October,2000'))
+    browser.element('[class="table-responsive"]').should(have.text('Maths, Computer Science'))
+    browser.element('[class="table-responsive"]').should(have.text('Sports, Reading, Music'))
+    browser.element('[class="table-responsive"]').should(have.text('Octopus.jpg'))
+    browser.element('[class="table-responsive"]').should(have.text('Moscow, Bulkina 65-39'))
+    browser.element('[class="table-responsive"]').should(have.text('NCR Delhi'))
